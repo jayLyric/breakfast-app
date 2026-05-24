5677732b1febdb6232a31e69dfc9a552f0cc9d84
Breakfast Admin <breakfast@example.com>
2026-05-24 20:25:15 +0800

    add legacy browser support for WeChat

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import legacy from '@vitejs/plugin-legacy';
 
 export default defineConfig({
   base: './',
-  plugins: [react()],
+  plugins: [
+    react(),
+    legacy({
+      targets: ['defaults', 'iOS >= 9', 'Android >= 4.4'],
+    }),
+  ],
 });
